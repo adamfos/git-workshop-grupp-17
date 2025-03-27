5f71398e05539099a4d20a10332d1ef5231d1840
Yasminelhaddaoui <yasmin_sosso@hotmail.com>
2025-03-27 09:59:06 +0100

    changes

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import yasmin, adam, Abdi
+import yasmin, adam, abdi, hq
 
 name = input("Enter your name: ")
 
 if name == "yasmin":
     yasmin.hello()
+elif name == "adam":
+    adam.hello()
+elif name == "abdi":
+    abdi.hello()
+elif name == "hq":
+    hq.hello()
